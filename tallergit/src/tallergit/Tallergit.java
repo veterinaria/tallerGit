0dcdd5ad0e20ab3beb5a6be2c70befb15220484f
camila <camopazo@alumnos.ubiobio.cl>
2014-04-07 16:31:28 -0300

    primer commit

--- tallergit/src/tallergit/Tallergit.java
+++ tallergit/src/tallergit/Tallergit.java
@@ -17,7 +17,7 @@ public class Tallergit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Mi primer commit");
+        System.out.println("Mi segundo commit");
     }
     
 }
